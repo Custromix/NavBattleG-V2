7ee08c0a0048561eb8495c067285de1134dcb54e
Custromix <kadersabi.106@gmail.com>
2023-04-28 03:02:57 +0200

    Update Server.cpp

--- Project2/Server.cpp
+++ Project2/Server.cpp
@@ -194,7 +194,6 @@ LRESULT CALLBACK Server::MainWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM
                         OutputDebugStringA(buffer.c_str());
                         OutputDebugStringA("\n");
                         pServer->ProtocolExecuter(pServer->Parser(buffer));
-                        pServer->ProtocolExecuter(*pServer->Parser(buffer));
                     }
 
                     // Receive data from the socket in wParam
